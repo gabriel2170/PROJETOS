17e7955daafdf7d6701228c2f0a29882d5d47ccc
GABRIEL SILVA LAMBOGLIA <64446258+gabriel2170@users.noreply.github.com>
2022-10-21 20:40:54 -0300

    Add files via upload

---
+++ JAVA/Convertendo Numeros.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    double preco = 1.55d;
+    
+    int num = (int) preco;
+    
+    System.out.println(num);
+    
+    
+    
+    double numDupli = 15.8858d;
+    
+    float numFloat = (float) numDupli;
+    
+    System.out.println(numFloat);
+    
+    
+  }
+}
---
+++ JAVA/validando datas de aniversario por nome.java
@@ -0,0 +1,30 @@
+public class Main {
+  public static void main(String[] args) {
+   	
+    
+    String nome = "";
+    
+    
+    switch (nome){
+    
+    	case "gabriel":
+        	System.out.print("Faz Aniversario em Janeiro!");
+           	break;
+            
+        case "Joana":
+        	
+            System.out.print("Faz Aniversario em Agosto!");
+            break;                  
+        	
+        default:
+        
+        System.out.print("Este nao é um nome valido!");
+        
+        break;
+        
+    
+    }
+    
+   
+  }
+}
